1eb733ea1f2b34c1f45e7224f121735cfbd043cd
joshpetrick <petrick.josh@gmail.com>
2026-07-05 23:28:57 -0400

    Align tab stock layouts and dividends

---
+++ src/main/java/local/portfolio/PortfolioCalculatorApplication.java
@@ -0,0 +1,11 @@
+package local.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioCalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioCalculatorApplication.class, args);
+    }
+}
